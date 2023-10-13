4dd4022957fc77b3089e9c24f518d1f630a8b329
Gyanendar <gyantiwari10101@gmail.com>
2023-10-13 19:10:39 +0530

    modified tank

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import './Home.css';
+// import './Home.css';
 
 const Home = () => {
     return (
-        <div className='flex flex-row Home'>
-            <div className='flex flex-col justify-between h-[100vh] w-[30vh] bg-transparent border-4 border-blue-300 text-white'>
+        <div className='flex flex-row '>
+            <div className='flex flex-col justify-between bg-[#000] h-[100vh] w-[30vh] bg-transparent border-4 border-white text-white'>
                 <div className='font-bold text-4xl mt-6'><h1>Reflow</h1></div>
                 <div className=''>
                     <ul className='flex flex-col justify-center items-center leading-8'>
@@ -31,26 +31,26 @@ const Home = () => {
                 <div className='flex flex-col'>
 
                     <div className='flex flex-col '>
-                        <div className='flex flex-col mt-6 ml-6 justify-between border-4 border-blue-300'>
+                        <div className='flex flex-col  justify-between bg-black border-4 border-white '>
                             <ul className='flex flex-row justify-between  w-full  p-3 text-white'>
                                 <li className='flex w-[15vh]'>Tanks</li>
-                                <li>Chemical Quantity</li>
-                                <li>Chemical Name</li>
-                                <li>Chemical Height</li>
-                                <li>Tank Height</li>
+                                <li className='flex w-[15vh]'>Chemical Quantity</li>
+                                <li className='flex w-[15vh]'>Chemical Name</li>
+                                <li className='flex w-[15vh]'>Chemical Height</li>
+                                <li className='flex w-[15vh]'>Tank Height</li>
+                            </ul>
+                        </div>
+                        <div className='flex flex-col  justify-between bg-white border-4 border-white '>
+                            <ul className='flex flex-row justify-between  w-full  p-3 text-black'>
+                                <li className='flex  ml-3 w-[15vh] h-[5vh] overflow-hidden border-2 border-black'><div  className='flex h-[5vh] w-[7.5vh]  bg-blue-400 text-gray-950 justify-center items-center'>50%</div></li>
+                                <li className='flex w-[15vh]'>50%</li>
+                                <li className='flex w-[15vh]'>Liquid Nitrogen</li>
+                                <li className='flex w-[15vh]'>5.6M</li>
+                                <li className='flex w-[15vh]'>10M</li>
                             </ul>
                         </div>
 
-                        <div className=' flex flex-row text-white'>
-                            <div className='flex bg-white h-[30vh] w-[20vh] m-5 rounded-3xl text-white border-4 border-black'>
-                                <div className=' w-[20vh] h-[15vh] bg-black rounded-t-2xl '>
-                                </div>
-                            </div>
-                            <div className='flex ml-[35vh] mt-[15vh]'>50%</div>
-                            <div className='flex ml-[35vh] mt-[15vh]'>Liquid Nitrogen</div>
-                            <div className='flex ml-[35vh] mt-[15vh]'>5.6 M</div>
-                            <div className='flex ml-[35vh] mt-[15vh]'>10 M</div>
-                        </div>
+                        
 
                         {/* <div className='flex flex-row mt-6 ml-6 justify-between'>
                             <p className='font-semibold text-md'>Previouo Page</p>
